035b569f26d364b767d5f2b08d3c6afad6167d8b
zakaria ridadarajat <zakariaridadarajat@gmail.com>
2025-10-03 23:08:00 +0200

    Switch Dash server launch to app.run

--- shapash/explainer/smart_explainer.py
+++ shapash/explainer/smart_explainer.py
@@ -1139,7 +1139,7 @@ class SmartExplainer:
                 port = 8050
             host_name = get_host_name()
             server_instance = CustomThread(
-                target=lambda: self.smartapp.app.run_server(debug=False, host=host, port=port)
+                target=lambda: self.smartapp.app.run(debug=False, host=host, port=port, use_reloader=False)
             )
             if host_name is None:
                 host_name = host
